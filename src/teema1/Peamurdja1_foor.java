997ccd0868f871ff1ff8899d0d634c81a7f87701
Jürgen Lätte <jlatte@klass2-16-l.intra.itcollege.ee>
2015-10-10 17:24:09 +0300

    valgusfoori kood

--- src/teema1/Peamurdja1_foor.java
+++ src/teema1/Peamurdja1_foor.java
@@ -1,5 +1,7 @@
 package teema1;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
 import lib.Foor;
 
 /**
@@ -17,5 +19,43 @@ import lib.Foor;
  * NB! Kui kirjutad new Foor(); siis IDE pakub sulle Foori importimise võimalust.
  * Kliki punase teksti peale ja vajuta alt+enter.
  */
-public class Peamurdja1_foor {
+public class Peamurdja1_foor extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+
+
+
+
+        Foor foor = new Foor();
+
+        int count = 0;
+        while (count< 10) {foor.vahetaPunast();
+            foor.paus(0.5);
+            foor.kustutaPunane();
+            foor.vahetaKollast();
+            foor.paus(0.5);
+            foor.kustutaKollane();
+            foor.vahetaRohelist();
+            foor.paus(2);
+            foor.vahetaRohelist();
+            foor.paus(0.5);
+            foor.vahetaRohelist();
+            foor.paus(0.5);
+            foor.vahetaRohelist();
+            foor.paus(0.5);
+            foor.vahetaRohelist();
+            foor.paus(0.5);
+            foor.vahetaRohelist();
+            foor.paus(0.5);
+            foor.vahetaKollast();
+            foor.paus(0.5);
+            foor.kustutaKollane();
+            foor.vahetaPunast();
+            foor.paus(3);
+            count = count +1;
+        }
+
+
+    }
 }
